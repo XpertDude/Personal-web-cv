7785627bb40004ac241b572ee60be88f6a2c88d0
XpertGenius <168342678+XpertDude@users.noreply.github.com>
2024-09-14 12:47:15 +0100

    main.js

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const menuBtn = document.querySelector('.btn');
 const menuDialog = document.getElementById('ul-menu');
 const list = document.querySelector('.hidden');
-console.log(menuDialog);
 
 const effectDialog = (eff) => {
     eff.style.opacity = '1';
@@ -10,7 +9,7 @@ const effectDialog = (eff) => {
 }
 const resetDialog = (eff) => {
     eff.style.opacity = '0';
-    eff.style.transform = 'scaleX(0)'; 
+    eff.style.transform = 'scaleX(0)';
     document.body.style.filter = 'none';
 };
 menuBtn.addEventListener('click', (event) => {
@@ -27,4 +26,37 @@ document.body.addEventListener('click', (event) => {
 
 list.addEventListener('click', (event) => {
     event.stopPropagation();
+})
+
+// email sending
+const messageDialog = document.getElementById('notification');
+const message = document.getElementById('message');
+const okBtn = document.getElementById('ok-btn');
+emailjs.init('9ECYgoX2aOx9R2vRp');
+function effectMessageOpen(eff) {
+    eff.style.opacity = '1';
+    eff.style.transform = 'scale(1)';
+}
+
+function effectMessageClose(eff) {
+    eff.style.opacity = '0';
+    eff.style.transform = 'scale(0)';
+}
+document.getElementById('contact-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    emailjs.sendForm('service_lo0s2rg', 'template_5ksjnna', this)
+        .then(function (response) {
+            if (response.status === 200) {
+                messageDialog.showModal();
+                effectMessageOpen(messageDialog);
+                message.textContent = 'The message was successfully sent';
+            }
+        }, function (error) {
+            messageDialog.showModal();
+            message.textContent = 'The message was not sent. Please try again later.';
+        });
+});
+okBtn.addEventListener('click', () => {
+    messageDialog.close();
+    effectMessageClose(messageDialog);
 })
